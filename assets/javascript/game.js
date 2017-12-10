6fd88f243dc503e03340a4296d772c82b1468096
Beza <bezawit.teferi@gmail.com>
2017-12-09 23:13:44 -0500

    the skeleton of the page is updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// eveytime the game restarts thre is a random number generated
+
+// There is a randomly generated number we want to match to win //
+// this sumber needs to be generated between 19 - 100
+
+// there are four crystals. when clicked they generate random numbers
+
+// there should be a variable that holds the number that are clicked on at the botom.
+// there should be a varible that holds the random number at the top.
+
+// these two should be disllaied to the html later on//
+
+// these two need to be compared and I need to crate a if / else to display and updated ein and lose //
