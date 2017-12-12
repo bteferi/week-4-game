02579359bc40aaa74423b24c510f8cfa120b3c6d
Beza <bezawit.teferi@gmail.com>
2017-12-11 21:08:02 -0500

    This code works but it is the long way ...Alot of code - First working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,67 @@
-
-// var win=0;
-// var losses=0;
-// var crystal_1;
-// var crystal_2;
-// var crystal_3;
-// var crystal_4;
-// eveytime the game restarts thre is a random number generated
-
 // this is my on click for the game to start and to genrate a random number//
 // Here we created an on-click event that responds to button clicks of the crystal image.
-var random_Comp_Number=50 // Computers guess - need a for loop to crate a random number everytime the game restarts
+var random_Comp_Number=Math.floor(Math.random()*120)+13;
+console.log(random_Comp_Number);
+// Computers guess - need a for loop to crate a random number everytime the game restarts
 $("#computer_guess").text(random_Comp_Number);
 
-var Crystal_Value =0; // this is an actual counter
-// my counter is starting at zero
+var current_guess_sum =0; // This is gone be displayed at the bottom..Sum of current guess
+var win=0;
 
-var Crystal_number_option =[1,2,3,4,5,6,7,8,9,10,11,12];
-var  random_crystal_value =Crystal_number_option[Math.round(Math.random())];
+var loses=0;
+
+var  random_crystal_value =Math.floor(Math.random()*12)+1;
+//===============================================================//
+///crate a custom attr to hold a value for each Crystal//
+
+$("#crystal_1").attr("data-crystal_Value",Math.floor(Math.random()*12)+1);
+
+$("#crystal_2").attr("data-crystal_Value",Math.floor(Math.random()*12)+1);
+
+$("#crystal_3").attr("data-crystal_Value",Math.floor(Math.random()*12)+1);
+
+$("#crystal_4").attr("data-crystal_Value",Math.floor(Math.random()*12)+1);
+//
+// $(".crystal").each(function(){
+//   console.log($(this).attr("data-crystal_Value"));
+// });
 
 
-$("#crystal_1").on("click", function() {
-  Crystal_Value+=random_crystal_value; // This will generate a random number everytime the crystal is clicke
-  // Clicking the button triggers an alert message.
-  alert("Your new score is: " + Crystal_Value); // this needs to show at the bottom for what you have so far at the bottom
-// you can maybe crate a for loop for this to keep adding the number
-  if(Crystal_Value===random_Comp_Number){
-    alert("you win");
+//=========================================================//
+$(".crystal").on("click", function() {
+
+ // this needs to show at the bottom for what you have so far at the bottom
+var current_signle_guess =($(this).attr("data-crystal_Value"));
+// this will update the sum at the bottom - it will add on to what ever image is clicked plus next click
+current_signle_guess = parseInt(current_signle_guess); // here i am changing the single guess to Intg.
+
+  current_guess_sum=current_guess_sum+current_signle_guess;
+  $(".Current_guess").text(current_guess_sum);
+
+  // ========================================================//
+
+
+  if(current_guess_sum===random_Comp_Number){
+    win=win+1;
+    $("#win").text(win);
+    current_guess_sum=0; // resetting my sum
+
   } // and as we update win variable by+1
-  else if(Crystal_Value >=random_Comp_Number){
-    alert(" you lose!")
+  else if(current_guess_sum >=random_Comp_Number){
+    loses=loses+1;
+    current_guess_sum=0;
+
+     // resetting game at 0 for Losses
+  if(loses>5){
+    loses=0;
+    win=0;
+
+  }
+// display Losses
+  $("#loses").text(loses);
+  $("#win").text(win);
   } // update the loses variable by +1
+
 
 });
 
